08fde76f36b54bd5bf43b9aaf7170f3345113a1d
K1NG9035 <ved.mistry99@gmail.com>
2025-08-08 23:09:10 +0530

    Delete routes directory

--- routes/showRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/showRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getShowDetails } = require('../controllers/showController');
-
-// GET /show-details
-router.get('/show-details', getShowDetails);
-
-module.exports = router;
